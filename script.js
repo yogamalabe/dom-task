ead1c3ead4177734594908acaf07e11e51b18735
yogamalabe <124970337+yogamalabe@users.noreply.github.com>
2023-03-13 12:17:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function foo(){
+    var fname=document.getElementById("fname").value;
+    console.log(fname);
+
+    var mname=document.getElementById("mname").value;
+    console.log(mname);
+
+    var lname=document.getElementById("lname").value;
+    console.log(lname);
+
+    var email=document.getElementById("email").value;
+    console.log(email);
+
+    var pword=document.getElementById("password").value;
+    console.log(pword);
+    
+    }
